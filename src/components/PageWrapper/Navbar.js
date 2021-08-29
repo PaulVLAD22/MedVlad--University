c0e6c2c459fd55570c2e5c943d2978dd62863325
paulvlad22 <paulvlad34@gmail.com>
2021-08-30 01:58:51 +0300

    context is null when dashboard is loaded

--- src/components/PageWrapper/Navbar.js
+++ src/components/PageWrapper/Navbar.js
@@ -1,133 +1,138 @@
-import{
-	Button,
-	Flex,
-	Menu,
-  Text
-} from "@chakra-ui/react";
+import { Button, Flex, Menu, Text, Box } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router";
 import { UserContext } from "../../App";
 import { backgroundImageGradient, secondaryColor } from "../utils/colors";
-import {AiOutlineBars} from 'react-icons/ai'
-
-export const Navbar = ({toggle}) => {
-	const history = useHistory();
-
-	const context = useContext(UserContext);
-
-	const goHome = () => {
-		history.push("/");
-	};
+import { AiOutlineBars } from "react-icons/ai";
+import { primaryFont } from "../utils/colors";
+export const Navbar = ({ toggle }) => {
+  const history = useHistory();
   
+  const context = useContext(UserContext);
+  console.log(context)
+
+  const goHome = () => {
+    history.push("/");
+  };
+
   const goToChat = () => {
     history.push("/chat");
-  }
+  };
   const goToMail = () => {
     history.push("/mail");
-  }
-  const goToInfostation = () =>{
+  };
+  const goToInfostation = () => {
     history.push("/infostation");
-  }
+  };
 
-	const handleLogout = () => {
-		context.logOut();
-		history.push("/");
-	};
-	return (
-		<Flex
-      
+  const handleLogout = () => {
+    context.logOut();
+    history.push("/");
+  };
+  return (
+    <Flex
       background={backgroundImageGradient}
-			width="100vw"
-			height="60px"
-			borderBottom="0.5px solid black"
-			justifyContent="space-between"
-			alignItems="center"
+      width="100vw"
+      height="70px"
+      borderBottom="0.5px solid black"
+      justifyContent="space-between"
+      alignItems="center"
       paddingX="5"
-     
-      >
-			<LeftNavMenu
-      >
-      
-				<Button 
-        background="white"
-        colorScheme={secondaryColor} mx={3} onClick={goHome}
+    >
+      <LeftNavMenu>
+        <Button
+          background="white"
+          colorScheme={secondaryColor}
+          mx={3}
+          onClick={goHome}
           fontFamily="sans-serif"
-						fontSize="20"
-						fontWeight="bold"
-						color="black"
-						alignSelf="center"
-            >
-						Home
-				</Button>
+          fontSize="20"
+          fontWeight="bold"
+          color="black"
+          alignSelf="center"
+        >
+          Home
+        </Button>
 
-        <Button 
-        background="white"
-        colorScheme={secondaryColor} mx={3} onClick={goToInfostation}
+        <Button
+          background="white"
+          colorScheme={secondaryColor}
+          mx={3}
+          onClick={goToInfostation}
           fontFamily="sans-serif"
-						fontSize="20"
-						fontWeight="bold"
-						color="black"
-						alignSelf="center"
-            >
-						Infostation
-				</Button>
-        <Button 
-        background="white"
-        colorScheme={secondaryColor} mx={3} onClick={goToChat}
+          fontSize="20"
+          fontWeight="bold"
+          color="black"
+          alignSelf="center"
+        >
+          Infostation
+        </Button>
+        <Button
+          background="white"
+          colorScheme={secondaryColor}
+          mx={3}
+          onClick={goToChat}
           fontFamily="sans-serif"
-						fontSize="20"
-						fontWeight="bold"
-						color="black"
-						alignSelf="center"
-            >
-						Live Chat
-				</Button>
-        <Button 
-        background="white"
-        colorScheme={secondaryColor} mx={3} onClick={goToMail}
+          fontSize="20"
+          fontWeight="bold"
+          color="black"
+          alignSelf="center"
+        >
+          Live Chat
+        </Button>
+        <Button
+          background="white"
+          colorScheme={secondaryColor}
+          mx={3}
+          onClick={goToMail}
           fontFamily="sans-serif"
-						fontSize="20"
-						fontWeight="bold"
-						color="black"
-						alignSelf="center"
-            >
-						Mail 
-				</Button>
-			</LeftNavMenu>
+          fontSize="20"
+          fontWeight="bold"
+          color="black"
+          alignSelf="center"
+        >
+          Mail
+        </Button>
+      </LeftNavMenu>
 
-			<Menu>
-				<Flex justifyContent="space-between"
-        alignItems="center">
-            <Text
-            fontSize="20"
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        
+      >
+        <Box width="100%">
+          <Text
+            fontSize="2xl"
             color="black"
             padding="3"
             px="5"
             fontWeight="bold"
-            fontFamily="sans-serif">
-              Hello{" "}{context.userInfo.username}
-            </Text>
-						<Button
-							fontSize="18px"
-							width="100px"
-							display="flex"
-							justifyContent="center"
-							onClick={handleLogout}>
-							Log out
-						</Button>
-            <MobileIcon onClick={toggle}>
-            <AiOutlineBars/>
-            </MobileIcon>
-				</Flex>
-
-			</Menu>
-		</Flex>
-	);
+            fontFamily={primaryFont}
+            letterSpacing="wider"
+          >
+            Hello {context.userInfo.username}
+          </Text>
+        </Box>
+        <Button
+          fontSize="18px"
+          width="100px"
+          display="flex"
+          justifyContent="center"
+          onClick={handleLogout}
+        >
+          Log out
+        </Button>
+        <MobileIcon onClick={toggle}>
+          <AiOutlineBars />
+        </MobileIcon>
+      </Flex>
+    </Flex>
+  );
 };
 
 export const MobileIcon = styled.div`
-display: none;
+  display: none;
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
@@ -138,8 +143,9 @@ display: none;
     cursor: pointer;
     color: white;
   }
-`
+`;
 export const LeftNavMenu = styled.div`
-@media screen and (max-width: 768px) {
+  @media screen and (max-width: 768px) {
     display: none;
-}`
+  }
+`;
