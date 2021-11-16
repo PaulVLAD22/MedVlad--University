f1f6e01505e211b1e438978886ea791afc847a47
Vlad Munteanu <paulvlad34@gmail.com>
2021-11-16 18:00:42 +0200

    starting working on chat page

--- src/components/pages/ChatPage.js
+++ src/components/pages/ChatPage.js
@@ -1,10 +1,36 @@
-import { Center, Flex } from "@chakra-ui/react";
+import { Center, Flex, Button } from "@chakra-ui/react";
+import SockJsClient from 'react-stomp';
 import ChatBox from "../ChatBox"
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+const SOCKET_URL = 'http://localhost:8080/chat';
+
 const ChatPage = () => {
-  return (
-    <Center width="100%" height="100%">
-      <ChatBox></ChatBox>
-    </Center>
-  );
-};
-export default ChatPage;
+  
+    const [message, setMessage] = useState('You server message here.');
+
+    let onConnected = () => {
+      console.log("Connected!!")
+    }
+
+    let onMessageReceived = (msg) => {
+      setMessage(msg.message);
+    }
+
+    return (
+      <Center width="100%" height="100%">
+        {/* <Button>Connect</Button>
+      <ChatBox></ChatBox> */}
+        <SockJsClient
+          url={SOCKET_URL}
+          topics={['/topic/public']}
+          onConnect={onConnected}
+          onDisconnect={console.log("Disconnected!")}
+          onMessage={msg => onMessageReceived(msg)}
+          debug={false}
+        />
+        <div>{message}</div>
+      </Center>
+    );
+  };
+  export default ChatPage;
