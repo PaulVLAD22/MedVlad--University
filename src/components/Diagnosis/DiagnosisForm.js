1f3622d8c1786b3f77d671352e14c7b38720aeb6
Vlad Munteanu <paulvlad34@gmail.com>
2021-12-05 01:35:33 +0200

    working on diagnosis result

--- src/components/Diagnosis/DiagnosisForm.js
+++ src/components/Diagnosis/DiagnosisForm.js
@@ -14,7 +14,7 @@ const DiagnosisForm = () => {
     const [error, setError] = useState("")
     const [render, setRender] = useState(0)
     const [searchWord, setSearchWord] = useState("")
-    const [diagnosisResult,setDiagnosisResult] = useState([])
+    const [diagnosisResult, setDiagnosisResult] = useState([])
 
 
 
@@ -78,13 +78,49 @@ const DiagnosisForm = () => {
         newArray[index] = 1 - newArray[index];
         setSelectedSymptoms([...newArray])
     }
+    const resetSelectedSymptoms = () =>{
+        let arrayOfZeros = []
+                        for (let i = 0; i < symptoms.length; i++) {
+                            arrayOfZeros.push(0);
+                        }
+                        setSelectedSymptoms((old) => [...arrayOfZeros])
+    }
     const calculateCondition = async () => {
-        setDiagnosisResult([])
+        let url = "/user/calculateDiagnosis"
+
+        const config = {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                Authorization: "Bearer " + context.jwt,
+            },
+        };
+
+        await axios({
+            method: "POST",
+            url: url,
+            headers: config.headers,
+            data:{selectedSymptoms},
+        }).then(
+            (response) => {
+                console.log(response.data)
+                setDiagnosisResult(response.data)
+            },
+            async (getError) => {
+                if (getError.response.status === 401) {
+                    console.log("SE CHEAMA REFRESH TOKEN")
+                    context.refreshAuthToken();
+                    setRender(render + 1)
+                }
+                else {
+                    setError("Unknown Error")
+                }
+            }
+        );
     }
 
     return (
         <Flex width="min(1024px,100%)" height="60%" boxShadow="dark-lg" p="10" flexDir="column" alignItems="center" >
-            <Flex width="100%" position="relative" justifyContent="space-between" className="responsive-flex" height="50%" >
+            <Flex width="100%" position="relative" justifyContent="space-between" className="responsive-flex" height="60%" >
                 <Flex flexDir="column" height="400px" overflowY="auto">
                     <Text m="3" fontSize="lg" letterSpacing="wide" fontWeight="500">Select your Symptoms</Text>
                     <Input value={searchWord} onChange={(e) => { setSearchWord(e.target.value) }} mb="3"></Input>
@@ -95,7 +131,7 @@ const DiagnosisForm = () => {
                     })}
                 </Flex>
                 {selectedSymptoms.indexOf(1) != -1 &&
-                    <Flex  flexDir="column" height="100%" p="2" overflowY="auto">
+                    <Flex flexDir="column" height="100%" p="2" overflowY="auto">
                         {symptoms.map((symptom, index) => {
                             return selectedSymptoms[index] == 1 ?
                                 <SymptomLabel key={symptom.id} name={symptom.name} selected={false} /> :
@@ -104,8 +140,9 @@ const DiagnosisForm = () => {
                     </Flex>
                 }
             </Flex>
+            <Button width="30%" onClick={resetSelectedSymptoms} mb="3"> Reset Symptomps</Button>
             <Button width="30%" onClick={calculateCondition}> Calculate Condition</Button>
-            <DiagnosisResult diagnosisResult={diagnosisResult}/>
+            <DiagnosisResult diagnosisResult={diagnosisResult} />
         </Flex>
     )
 }
--- src/components/Diagnosis/DiagnosisResult.js
+++ src/components/Diagnosis/DiagnosisResult.js
@@ -1,11 +1,26 @@
+import { Flex, Text } from '@chakra-ui/layout'
 import React from 'react'
 
-const DiagnosisResult = ({diagnosisResult}) => {
+const DiagnosisResult = ({ diagnosisResult }) => {
     console.log(diagnosisResult)
     return (
-        <div>
-            
-        </div>
+        <Flex width="100%" height="90%" alignItems="center" justifyContent="space-between">
+            {diagnosisResult.condition == null ?
+                <Text fontSize="x-large" position="absolute" left="50%" transform="translate(-50%,0)">Calculation Results</Text>
+                :
+                <>
+                    <Text>
+                        {diagnosisResult.condition}
+                    </Text>
+                    <Text>
+                        Similarity Score : {diagnosisResult.similarityScore}
+                    </Text>
+                    <Text>
+                        Diagnostician Doctor: {diagnosisResult.similarQuestionDoctor.username}
+                    </Text>
+                </>
+            }
+        </Flex>
     )
 }
 
